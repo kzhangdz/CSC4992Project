a0e5acaaca75691283c2ceced8eb729de1f1b010
Kevin Zhang <ga6198@wayne.edu>
2019-02-07 21:54:28 -0500

    added preliminary card class

---
+++ card.py
@@ -0,0 +1,17 @@
+class Card:
+    def __init__(self):
+        self.isFaceUp = False
+        #self.image = 
+
+    '''def __init__(self, imageClass()):
+        self.isFaceUp = False
+        self.image = imageClass()'''
+
+    '''https://www.geeksforgeeks.org/working-images-python/'''
+
+    def display(self):
+        '''show card to user'''
+        pass
+
+newCard = Card()
+print(newCard.isFaceUp)
