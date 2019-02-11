6d628da5d2d91bef19814a56dd0b29c26db2e478
Kevin Zhang <ga6198@wayne.edu>
2019-02-10 22:00:40 -0500

    added preliminary for singlePlayerMenu

--- menu.py
+++ menu.py
@@ -21,11 +21,11 @@ class Menu(tk.Tk):
         #create menu frames/pages
         self.frames = {}
 
-        frame = HomeMenu(container, self)
-
-        self.frames[HomeMenu] = frame
-
-        frame.grid(row=0, column = 0, sticky="nsew")
+        #add new frames in this for loop
+        for eachFrame in (HomeMenu, SinglePlayerMenu):
+            frame = eachFrame(container, self)
+            self.frames[eachFrame] = frame
+            frame.grid(row=0, column = 0, sticky="nsew")
 
         self.showFrame(HomeMenu)
 
@@ -43,12 +43,26 @@ class HomeMenu(tk.Frame):
 
         #create single player button
         singlePlayerButton = tk.Button(self, text="Single Player",
-                                       command=lambda: controller.show_frame(SinglePlayerPage))
+                                       command=lambda: controller.showFrame(SinglePlayerMenu))
         singlePlayerButton.pack()
 
         #create two player button
+        #FIXME: add command to show frame
         twoPlayerButton = tk.Button(self, text="Two Player")
+        twoPlayerButton.pack()
 
+class SinglePlayerMenu(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Single Player", font=TITLE_FONT)
+        label.pack(pady=10, padx=10)
+
+        #create back button
+        backButton = tk.Button(self, text="Back",
+                                       command=lambda: controller.showFrame(HomeMenu))
+        backButton.pack()
+        
 
 if __name__ == "__main__":
     menus = Menu()
