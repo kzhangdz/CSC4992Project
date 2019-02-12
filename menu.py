4aab17eb6a4eb9ad439c28f6698d89d7cc6b5c99
ga6198 <47435894+ga6198@users.noreply.github.com>
2019-02-11 21:54:35 -0500

    added back button

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from os.path import join, realpath, dirname
 #from homemenu import HomeMenu
 
 #define default fonts
@@ -44,38 +45,53 @@ class HomeMenu(tk.Frame):
         tk.Frame.__init__(self, parent)
         label = ttk.Label(self, text="Memory Game", font=TITLE_FONT)
         label.pack(padx=20, pady=10, side=tk.TOP)
+        #label.grid(row=1,column=1)
 
         #create single player button
         singlePlayerButton = ttk.Button(self, text="Single Player",
                                        command=lambda: controller.showFrame(SinglePlayerMenu))
         singlePlayerButton.pack(padx=10, pady=10, side=tk.LEFT)
+        #singlePlayerButton.grid(row=3,column=1)
 
         #create two player button
         twoPlayerButton = ttk.Button(self, text="Two Player",
                                      command=lambda: controller.showFrame(TwoPlayerMenu))
         twoPlayerButton.pack(padx=10, pady=10, side=tk.RIGHT)
+        #twoPlayerButton.grid(row=4,column=1)
+
+        #FIXME: Switch to minimenu later (might be easier to use toolbar)
+        #twoPlayerButton.grid(row=6,column=2)
+        
 
 class SinglePlayerMenu(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Single Player", font=TITLE_FONT)
+        label = tk.Label(self, text="Single Player", font=TITLE_FONT)
         label.pack(padx=10, pady=10)
 
         #create back button
-        backButton = ttk.Button(self, text="Back",
+        backButton = tk.Button(self, text="Back",
                                        command=lambda: controller.showFrame(HomeMenu))
+
+        #getting image directory
+        #FIXME: use directoryParser class eventually
+        currentPath = dirname(realpath(__file__))
+        imagePath = join(currentPath, "images", "menu", "back_button.gif")
+        backImage = tk.PhotoImage(file=imagePath)
+        backButton.image=backImage #keep reference to photo
+        backButton.config(image=backImage)
         backButton.pack()
 
 class TwoPlayerMenu(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Two Player", font=TITLE_FONT)
+        label = tk.Label(self, text="Two Player", font=TITLE_FONT)
         label.pack(padx=10, pady=10)
 
         #create back button
-        backButton = ttk.Button(self, text="Back",
+        backButton = tk.Button(self, text="Back",
                                        command=lambda: controller.showFrame(HomeMenu))
         backButton.pack()
                 
