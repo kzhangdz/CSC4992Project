d7d677bf8a0839d7e47270055496db5b12c4c26c
Kevin Zhang <keevee810@gmail.com>
2019-03-07 18:05:15 -0500

    added user options in main page

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pygame
-from pygameEnvironment import *
-import menu
 
 if __name__ == "__main__":
-    menu.mainMenu()
+    "Start at main page, single player, or multiplayer?"
+    startPage = input("Enter main, single, multi, or quit: ")
+
+    if startPage == "single":
+        import menu
+        menu.singlePlayerMenu()
+    elif startPage == "multi":
+        import menu
+        menu.multiPlayerMenu()
+    elif startPage == "quit":
+        quit()
+    else:
+        import menu
+        menu.mainMenu()
     quit()
 
 
