6fb5b6c36600a30f7ca8462d2985266370b19052
Kevin Zhang <keevee810@gmail.com>
2019-04-19 21:43:31 -0400

    created final pdf

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
             else:
                 raise ValueError('Your input was not one of the options')
         except ValueError as err:
+            print(err)
             print('Start over')
 
     quit()
