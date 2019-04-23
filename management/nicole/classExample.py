f267124e93b1f69fbc81cfa952ca6e62f20fde98
Nicole Racovites <nraco44@gmail.com>
2019-04-22 21:43:32 -0400

    added my sections

---
+++ management/nicole/classExample.py
@@ -0,0 +1,12 @@
+class Person:
+
+    def __init__(self, firstName, lastName):
+        self.firstName = firstName
+        self.lastName = lastName
+        
+
+person = Person("Nicole", "Racovites")
+
+print(person.firstName)
+print(person.lastName)
+    
---
+++ management/nicole/listExample.py
@@ -0,0 +1,6 @@
+with open("names.txt", "r") as ins:
+    names = []
+    for line in ins:
+        names.append(names)
+
+
